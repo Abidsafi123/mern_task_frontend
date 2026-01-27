4cbf3bdf50d8d8f27dbdd4c5056cda516593a8a6
= <=>
2026-01-27 11:38:22 +0500

    created stroe

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import authReducer from "../features/auth/authSlice.js";
 import taskReducer from "../features/task/taskSlice.js"
+import adminReducer from "../features/admin/adminSlice.js"
 export const store = configureStore({
     reducer:{
     auth:authReducer,
     task:taskReducer,
+    admin:adminReducer
     }
 })
--- src/features/admin/adminSlice.js
+++ src/features/admin/adminSlice.js
@@ -1,6 +1,7 @@
 import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
 
 import adminService from "./adminService"
+import { act } from "react"
 const initialState = {
     users:[],
     isLoading:false,
@@ -21,3 +22,34 @@ export const fetchAllUsers = createAsyncThunk('admin/fetchAllUsers',async(_,thun
         
     }
 })
+
+export const adminSlice = createSlice({
+    name:'admin',
+    initialState,
+    reducers:{
+        reset:(state)=>{
+             initialState
+        }
+
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchAllUsers.pending,(state)=>{
+            state.isLoading = true
+        })
+        builder.addCase(fetchAllUsers.fulfilled,(state,action)=>{
+            state.isLoading = false,
+            state.isError = false,
+            state.isSuccess = true,
+            state.users = action.payload,
+            state.message = action.payload
+        })
+        builder.addCase(fetchAllUsers.rejected,(state,action)=>{
+            state.isError = true,
+            state.isLoading = false,
+            state.message = action.payload
+
+        })
+    }
+})
+export const {reset} = adminSlice.actions;
+export default adminSlice.reducer
