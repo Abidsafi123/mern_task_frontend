1259ed3a3af3962135f9b42cdeb24454a7df8b7e
= <=>
2026-01-27 11:53:27 +0500

    adminDashboard modified

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,14 +1,9 @@
 import React,{useEffect,useState} from 'react'
-import {useSelector,useDispatch} from "react-redux"
+ 
 
 
 const adminDashboard = () => {
-    const dispatch = useDispatch()
-    
-
-    useEffect(()=>{
-         
-    })
+   
   return (
     <div>adminDashboard</div>
   )
